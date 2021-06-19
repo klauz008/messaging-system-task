96367de9cefeb83907a2aab843b2f34ee69f31bf
Nicholas Bassey <nickyawat@gmail.com>
2021-06-19 07:58:56 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+function sendMessage() {
+    let message = document.getElementById("message-box");
+    let html = "<div class="Message-boxmy-message-box">" + "<div class="message my-message"> "  + message + " </div>" + "<div class="seperator"></div>" + "</div>";
+
+}
